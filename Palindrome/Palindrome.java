0c717ba19a9954f0efe59d7a615aacdf94c2607d
aabdulhu <ali.almoumnin@gmail.com>
2026-04-11 21:56:51 +0300

    java-intro

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -6,7 +6,9 @@ public class Palindrome {
         int left = 0;
         int right = s.length() - 1;
         while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
+            char l = Character.toLowerCase(s.charAt(left));
+            char r = Character.toLowerCase(s.charAt(right));
+            if (l != r) {
                 return false;
             }
             left++;
