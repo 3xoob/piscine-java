56263b7d23945beeaf0c1242464af74446f96b3c
aabdulhu <ali.almoumnin@gmail.com>
2026-04-11 21:55:06 +0300

    java-intro

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -1,5 +1,8 @@
 public class StringConcat {
     public static String concat(String s1, String s2) {
+        if (s1 == null && s2 == null) {
+            return null;
+        }
         String left = s1 == null ? "" : s1;
         String right = s2 == null ? "" : s2;
         return left + right;
