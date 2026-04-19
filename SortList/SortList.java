5e614742f779a905910c13774e29a56f76aef616
aabdulhu <ali.almoumnin@gmail.com>
2026-04-19 17:10:14 +0300

    java-structures

--- SortList/SortList.java
+++ SortList/SortList.java
@@ -5,12 +5,18 @@ import java.util.List;
 public class SortList {
 
     public static List<Integer> sort(List<Integer> list) {
+        if (list == null) {
+            return null;
+        }
         List<Integer> sorted = new ArrayList<>(list);
         Collections.sort(sorted);
         return sorted;
     }
 
     public static List<Integer> sortReverse(List<Integer> list) {
+        if (list == null) {
+            return null;
+        }
         List<Integer> sorted = new ArrayList<>(list);
         sorted.sort(Collections.reverseOrder());
         return sorted;
