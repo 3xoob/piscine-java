931eb63c68c5a11ed6f842c87ccd7f24dc19d60e
aabdulhu <ali.almoumnin@gmail.com>
2026-05-02 17:25:09 +0300

    standard-lib

--- ParseDate/ParseDate.java
+++ ParseDate/ParseDate.java
@@ -2,21 +2,43 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Locale;
 
 public class ParseDate {
 
     public static LocalDateTime parseIsoFormat(String stringDate) {
-        return LocalDateTime.parse(stringDate);
+        if (stringDate == null) {
+            return null;
+        }
+        try {
+            return LocalDateTime.parse(stringDate);
+        } catch (DateTimeParseException e) {
+            return null;
+        }
     }
 
     public static LocalDate parseFullTextFormat(String stringDate) {
+        if (stringDate == null) {
+            return null;
+        }
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE d MMMM yyyy", Locale.FRENCH);
-        return LocalDate.parse(stringDate, formatter);
+        try {
+            return LocalDate.parse(stringDate, formatter);
+        } catch (DateTimeParseException e) {
+            return null;
+        }
     }
 
     public static LocalTime parseTimeFormat(String stringDate) {
+        if (stringDate == null) {
+            return null;
+        }
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh 'heures du' a, mm 'minutes et' ss 'secondes'", Locale.FRENCH);
-        return LocalTime.parse(stringDate, formatter);
+        try {
+            return LocalTime.parse(stringDate, formatter);
+        } catch (DateTimeParseException e) {
+            return null;
+        }
     }
 }
